aefac8456b8d5a9457d3e57119352a551b05ebe9
Sean Nakamura <seanenakamura@gmail.com>
2015-07-19 22:30:23 -1000

    img desc successfully extracts

--- extractor.js
+++ extractor.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-  alert('hello');
+  // alert('hello');
   document.getElementById("image").onclick = function() {
-      EXIF.getData(this, function() {
-          alert(EXIF.pretty(this));
-      });
-  }
+    EXIF.getData(this, function(){
+      console.log(EXIF.getTag(this, "ImageDescription"));
+    });
+  };
 };
